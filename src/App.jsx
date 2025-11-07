55af2b2c3632e2250b054780a58166f439a27f91
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-07 20:54:00 +0100

    app with dynamic language selection and style with css

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import { useState } from "react";
 
 function App() {
   const [selectedId, setSelectedId] = useState(languages[0].id);
-  const selectedLanguage = languages.find((linguaggio) => linguaggio.id === selectedId);
+  const selectedLanguage = languages.find(
+    (linguaggio) => linguaggio.id === selectedId
+  )
   return (
     <>
-      <h1>Learn Web development</h1>
-      <div>
-        {languages.map((linguaggio) => (
-          <button key={linguaggio.id} onClick={() => setSelectedId(linguaggio.id)}>
-            {linguaggio.title}
-          </button>
-        ))}
-      </div>
-      <div>
-        <h2>{selectedLanguage.title}</h2>
-        <p>{selectedLanguage.description}</p>
+      <div className="app-container">
+        <h1>Learn Web development</h1>
+        <div className="buttons-row">
+          {languages.map((linguaggio) => (
+            <button key={linguaggio.id} className={"lang-button" + (linguaggio.id === selectedId ? " active" : "")} onClick={() => setSelectedId(linguaggio.id)}>
+              {linguaggio.title}
+            </button>
+          ))}
+        </div>
+        <div className="card">
+          <h2>{selectedLanguage.title}</h2>
+          <p>{selectedLanguage.description}</p>
+        </div>
       </div>
     </>
   )
