3994ad1c0640be430da2a6b401262830e262674c
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-07 17:16:24 +0100

    static card for the first language info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import languages from "./components/languages"
 
 function App() {
+  const firstLinguaggio = languages[0];
   return (
     <>
       <h1>Learn Web development</h1>
@@ -8,6 +9,10 @@ function App() {
         {languages.map((lang) => (
           <button key={lang.id}>{lang.title}</button>
         ))}
+      </div>
+      <div>
+        <h2>{firstLinguaggio.title}</h2>
+        <p>{firstLinguaggio.description}</p>
       </div>
     </>
   )
