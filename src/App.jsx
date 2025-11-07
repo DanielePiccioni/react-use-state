d9e5fb4b0427e95766356df63b9e92aad23614ee
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-07 17:52:37 +0100

    add useState to switch displayed language when click other button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import languages from "./components/languages"
+import { useState } from "react";
 
 function App() {
-  const firstLinguaggio = languages[0];
+  const [selectedId, setSelectedId] = useState(languages[0].id);
+  const selectedLanguage = languages.find((linguaggio) => linguaggio.id === selectedId);
   return (
     <>
       <h1>Learn Web development</h1>
       <div>
-        {languages.map((lang) => (
-          <button key={lang.id}>{lang.title}</button>
+        {languages.map((linguaggio) => (
+          <button key={linguaggio.id} onClick={() => setSelectedId(linguaggio.id)}>
+            {linguaggio.title}
+          </button>
         ))}
       </div>
       <div>
-        <h2>{firstLinguaggio.title}</h2>
-        <p>{firstLinguaggio.description}</p>
+        <h2>{selectedLanguage.title}</h2>
+        <p>{selectedLanguage.description}</p>
       </div>
     </>
   )
